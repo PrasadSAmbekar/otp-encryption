ce3d1c48432db7715f5e087fc13060af31062d15
PrasadSAmbekar <116183086+PrasadSAmbekar@users.noreply.github.com>
2024-11-12 18:44:42 +0530

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,14 +5,14 @@ function AboutUsPage() {
         <div style={{ minHeight: 'calc(70vh - 65px - 55px)' }} className='mx-5'>
             <div className='my-5' />
             <div className='responsive-about google-fonts-roboto' style={{ textAlign: 'center' }}>
-                {/* Image in the center */}
-                <img src="/friends.jpg" alt="profile" style={{ width: '5', height: '5', margin: '0px auto' }} />
-                
-                {/* Some descriptive text below the image */}
                 <p style={{ marginTop: '20px', fontSize: '1.2rem' }}>
                     Welcome to our page where we explore the power of secure cryptographic solutions. 
                     This is a demonstration of how images and content can seamlessly be integrated in React.
                 </p>
+                {/* Image in the center */}
+                <img src="/friends.jpg" alt="profile" style={{ width: '20%', height: '10%', margin: 'auto' }} />
+                
+                {/* Some descriptive text below the image */}
             </div>
         </div>
     );
